0181d0ea28e7c73c121b5b8c3474bfbf60efd1f9
Robert Borghese <28355157+SomeRanDev@users.noreply.github.com>
2023-12-01 17:06:58 -0500

    Allow Run to generate bindings automatically

--- Run.hx
+++ Run.hx
@@ -1,8 +1,11 @@
 package;
 
+import sys.FileSystem;
+import sys.io.Process;
+
 function main() {
-	var jsonPath = "extension_api.json";
-	var outputDir = "godot_bindings";
+	var jsonPath = null;
+	var outputDir = null;
 
 	final args = Sys.args();
 
@@ -26,13 +29,13 @@ function main() {
 		case [cwd]: {
 			cwd;
 		}
-		case [_jsonPath, cwd]: {
-			jsonPath = _jsonPath;
+		case [_outputDir, cwd]: {
+			outputDir = _outputDir;
 			cwd;
 		}
-		case [_jsonPath, _outputDir, cwd]: {
-			jsonPath = _jsonPath;
+		case [_outputDir, _jsonPath, cwd]: {
 			outputDir = _outputDir;
+			jsonPath = _jsonPath;
 			cwd;
 		}
 		case _: {
@@ -42,25 +45,122 @@ function main() {
 		}
 	}
 
-	Sys.setCwd(cwd);
+	// Ensure we use current directory of haxelib run call.
+	if(cwd != null) {
+		Sys.setCwd(cwd);
+	}
 
+	// Check output directory
+	if(outputDir == null) {
+		Sys.println("No output directory defined, using \"godot_bindings/\".");
+		outputDir = "godot_bindings";
+	}
+
+	// Check json path
+	final shouldGenerate = if(jsonPath == null && !FileSystem.exists("extension_api.json")) {
+		Sys.println("No `extension_api.json` found, let's generated them!");
+		true;
+	} else {
+		false;
+	}
+
+	// Generate extension_api.json
+	if(shouldGenerate) {
+		jsonPath = "extension_api.json";
+		generateJson();
+	}
+
+	// Generate and output type definitions
 	final typeDefinitions = godot.Bindings.generate(jsonPath, {
 		cpp: isCpp,
 		nativeNameMeta: nativeMeta
 	});
 	godot.Bindings.output(outputDir, typeDefinitions);
+
+	// We done!!
+	Sys.println("Done!");
 }
 
 function help() {
 	Sys.println("====================================
 * Godot Bindings Generator for Haxe
 ====================================
-godot-api-generator [json-path] [output-directory]
+godot-api-generator [output-directory=godot_bindings] [json-path] 
 
-[Default arguments]
-godot-api-generator ./extension_api.json ./godot_bindings
+If no \"json-path\" is given, the bindings will be generated automatically.
 
 [Print this help message]
 godot-api-generator help
+
+[Add C++ Information]
+--cpp
 ====================================");
+}
+
+/**
+	Generates the `extension_api.json` file by asking for Godot path.
+**/
+function generateJson() {
+	// We start!!
+	Sys.println("Generating Godot bindings...");
+
+	// Check for godot executable
+	var godotVersion = "";
+	var godotPath: Null<String> = Sys.getEnv("GODOT_PATH");
+	if(godotPath == null) {
+		godotPath = try {
+			final process = new Process("godot", ["--version"]);
+			if(process.exitCode(true) == 0) {
+				"godot";
+			} else {
+				null;
+			}
+		} catch(e) {
+			null;
+		}
+	}
+
+	// Request path to godot executable if not found
+	if(godotPath == null) {
+		Sys.println("'godot' could not be found, please enter the path to the executable:");
+		Sys.print("> ");
+
+		while(true) {
+			final path = Sys.stdin().readLine().toString();
+
+			godotPath = if(FileSystem.exists(path) && !FileSystem.isDirectory(path)) {
+				try {
+					final process = new Process(path, ["--version"]);
+					if(process.exitCode(true) == 0) {
+						godotVersion = process.stdout.readAll().toString();
+						path;
+					} else {
+						null;
+					}
+					
+				} catch(e) {
+					null;
+				}
+			} else {
+				null;
+			}
+
+			if(godotPath == null) {
+				Sys.println("'" + path + "' could not be found, please try again:");
+				Sys.print("> ");
+			} else {
+				break;
+			}
+		}
+	}
+
+	final versionArray = godotVersion.split(".");
+	final is4_2orAbove = Std.parseInt(versionArray[0]) >= 4 && Std.parseInt(versionArray[1]) >= 2;
+
+	final dumpType = is4_2orAbove ? "--dump-extension-api-with-docs" : "--dump-extension-api";
+
+	// Generate `extension_api.json`
+	Sys.println('>> ${godotPath} ${dumpType} --headless');
+	final process = new Process(godotPath, [dumpType, "--headless"]);
+	process.exitCode(true);
 }
